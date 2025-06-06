ead364399fcc31115a9eceeb4ac4fc159a2eeecc
haseenice <haseenice@yeah.net>
2025-06-06 12:32:25 +0800

    commit files

---
+++ Master/Site.Master.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text;
+using System.Text.RegularExpressions;
+using EKETEAM.FrameWork;
+using EKETEAM.Data;
+
+namespace EKECMS.Master
+{
+    public partial class Site : System.Web.UI.MasterPage
+    {
+        protected eSiteInfo siteinfo;
+        public eSiteInfo SiteInfo
+        {
+            get { return siteinfo; }
+            set { siteinfo = value; }
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //this.Render(
+            if (SiteInfo == null) siteinfo = new eSiteInfo();
+            Head1.SiteInfo = SiteInfo;
+            Top1.SiteInfo = SiteInfo;
+            Left1.SiteInfo = SiteInfo;
+            CopyRight1.SiteInfo = SiteInfo;
+            LitSubTopAds.Text = SiteInfo.SubTopAds;
+        }
+        protected override void Render(HtmlTextWriter writer)
+        {
+            //zh-chs 简体中文
+            //zh_cht 簡體中文
+            string lang = eParameters.QueryString("lang");
+            System.IO.StringWriter stringWriter = new System.IO.StringWriter();
+            HtmlTextWriter htmlWriter = new HtmlTextWriter(stringWriter);
+            try
+            {
+                base.Render(htmlWriter);
+                htmlWriter.Close();
+                string content = stringWriter.ToString();
+                if (lang.ToLower() == "zh_cht")
+                {
+                    content = content.TraditionalChinese();
+                    content = content.Replace("簡體", "简体");
+                    content = Regex.Replace(content, ".aspx\\?", ".aspx?lang=zh_cht&", RegexOptions.IgnoreCase);
+                    content = Regex.Replace(content, "default.aspx", "Default.aspx?lang=zh_cht", RegexOptions.IgnoreCase);
+                    content = Regex.Replace(content, "lang=zh_cht&\\?lang", "lang", RegexOptions.IgnoreCase);
+                    content = Regex.Replace(content, "lang=zh_cht&lang", "lang", RegexOptions.IgnoreCase);
+                    content = Regex.Replace(content, "lang=zh_cht\\?lang", "lang", RegexOptions.IgnoreCase);
+                    
+                }
+                content = Pub.StaticURL(content);
+
+
+                writer.Write(content);
+            }
+            catch { }
+            finally
+            {
+                stringWriter.Dispose();
+                htmlWriter.Close();
+                htmlWriter.Dispose();
+            }
+        }
+    }
+}
---
+++ Phone/GuestBooks.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using EKETEAM.FrameWork;
+using EKETEAM.Data;
+
+namespace EKECMS.Phone
+{
+    public partial class GuestBooks : System.Web.UI.Page
+    {
+        public string id = "";
+        public string pid = "";
+        public eSiteInfo siteinfo = new eSiteInfo();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Pub.checkPhone(); //不是手机、ipod 访问则跳转到PC版
+
+            #region 安全性检查
+            if (Request.QueryString["id"] == null) Response.Redirect("default.aspx", true);
+            id = Request.QueryString["id"].ToString();
+            if (Request.QueryString["pid"] != null) pid = Request.QueryString["pid"].ToString();
+            #endregion
+
+            ((EKECMS.Master.Phone)Master).SiteInfo = siteinfo;
+            List();
+        }
+        private void List()
+        {
+            if (pid.Length > 0) eDataView1.Where.Add("ClassID in (" + pid + ",(select ClassID from eWeb_Class where ParentClassID='" + pid + "' and delTag=0))");
+            eDataView1.Where.Add("ColumnID='" + id + "'");
+            eDataView1.Where.Add("WebCode='" + eBase.getWebCode() + "'");
+
+
+        }
+    }
+}
