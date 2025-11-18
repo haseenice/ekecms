affde82029bf34264104d450ea075cd1711b46fa
haseenice <haseenice@yeah.net>
2025-11-18 11:54:40 +0800

    add file

---
+++ Controls/mCopyRight.ascx
@@ -0,0 +1,13 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="mCopyRight.ascx.cs" Inherits="EKECMS.Controls.mCopyRight" %>
+<%if(SiteInfo.Site["ContentTel"].ToString().Length>0){%>
+<div class="Mobile"><a href="tel:<%=SiteInfo.Site["ContentTel"].ToString().Replace("-","")%>">点击免费电话咨询:<%=SiteInfo.Site["ContentTel"].ToString()%></a></div>
+<%}%>
+<div id="copyright" style="margin-top:15px;">
+<div class="bottom_navigation"></div>
+<div class="bottom_copyright"><%=SiteInfo.Site["mFooter"].ToString()%></div>
+</div>
+<div id="tool">
+<ul class="toollist">
+<%=Tools%>
+</ul>
+</div>
---
+++ Customs/Default/UserInfo.aspx.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using EKETEAM.Data;
+using EKETEAM.FrameWork;
+
+
+
+    public partial class Customs_Default_UserInfo : System.Web.UI.Page
+    {
+        public eUser user;
+        public string act = eParameters.Request("act");
+        public string AppItem = eParameters.Request("AppItem");
+        public eModel model;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            user = new eUser("Application");
+            user.Check();
+            string ModelID = "5fcc3772-39be-4c7a-8c6e-9f64781407ae";
+            model = new eModel(ModelID, user);
+
+            model.Power["view"] = true; //给查看权限
+            model.Power["edit"] = true; //给添加权限
+            model.ID = user.ID;
+            model.eForm.onChange += eForm_onChange;
+            switch (act)
+            {
+                case "":
+                    model.Action = "view";
+                    //litBody.Text = model.getViewHTML();
+                    litBody.Text = model.getActionHTML();
+                    break;
+                case "edit":
+                     model.Action = "edit";
+                    //litBody.Text = model.getEditHTML();
+                     litBody.Text = model.getActionHTML();
+                    break;
+                case "save":
+                    //model.Save();
+                    model.autoHandle();
+                    break;
+            }
+        }
+
+        private void eForm_onChange(object sender, eFormTableEventArgs e)
+        {
+            if (e.eventType == eFormTableEventType.Updated)
+            {
+                DataTable tb = eBase.UserInfoDB.getDataTable("select * from a_eke_sysUsers where UserID='" + e.ID + "'");
+                if (tb.Rows.Count > 0)
+                {
+                    user["xm"] = tb.Rows[0]["xm"].ToString();
+                    if (tb.Rows[0]["face"].ToString().Length > 10)
+                    {
+                        user["face"] = eBase.getVirtualPath() + tb.Rows[0]["face"].ToString();
+                    }
+                    user.Save();
+                }
+            }
+        }
+        protected void Page_PreRender(object sender, EventArgs e)
+        {
+            if (Master == null) return;
+            Literal lit = (Literal)Master.FindControl("LitTitle");
+            if (lit != null) lit.Text = eConfig.ApplicationTitle(user["SiteID"].ToString()); // model.ModelInfo["mc"].ToString() + " - " +
+
+        }
+    }
